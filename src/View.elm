dab61a940fea9f5c07d57f120f6b319c3b1ab76d
Ashish Chandwani <ashish.s.chandwani@gmail.com>
2016-07-20 13:11:36 -0700

    Add view routing to Main module

--- src/View.elm
+++ src/View.elm
@@ -5,6 +5,9 @@ import Html.App
 import Messages exposing (Msg(..))
 import Models exposing (Model)
 import Players.List
+import Players.Edit
+import Players.Models exposing (PlayerId)
+import Routing exposing (Route(..))
 
 
 view : Model -> Html Msg
@@ -15,5 +18,34 @@ view model =
 
 page : Model -> Html Msg
 page model =
-    -- convert Player.Messages to PlayersMsg
-    Html.App.map PlayersMsg (Players.List.view model.players)
+    case model.route of
+        PlayersRoute ->
+            Html.App.map PlayersMsg (Players.List.view model.players)
+
+        PlayerRoute id ->
+            playerEditPage model id
+
+        NotFoundRoute ->
+            notFoundView
+
+
+playerEditPage : Model -> PlayerId -> Html Msg
+playerEditPage model playerId =
+    let
+        maybePlayer =
+            model.players
+                |> List.filter (\player -> player.id == playerId)
+                |> List.head
+    in
+        case maybePlayer of
+            Just player ->
+                Html.App.map PlayersMsg (Players.Edit.view player)
+
+            Nothing ->
+                notFoundView
+
+
+notFoundView : Html msg
+notFoundView =
+    div []
+        [ text "Not found" ]
