c3176f8ee02962a8cb34316764d5ed5a8395635c
Ashish Chandwani <ashish.s.chandwani@gmail.com>
2016-07-23 23:47:55 -0700

    Switch from Html.App.program to Navigation.program

--- src/Main.elm
+++ src/Main.elm
@@ -1,16 +1,21 @@
 module Main exposing (..)
 
-import Html.App
+import Navigation
 import Messages exposing (Msg(..))
 import Models exposing (Model, initialModel)
 import View exposing (view)
 import Update exposing (update)
 import Players.Commands exposing (fetchAll)
+import Routing exposing (Route)
 
 
-init : ( Model, Cmd Msg )
-init =
-    ( initialModel, Cmd.map PlayersMsg fetchAll )
+init : Result String Route -> ( Model, Cmd Msg )
+init result =
+    let
+        currentRoute =
+            Routing.routeFromResult result
+    in
+        ( initialModel currentRoute, Cmd.map PlayersMsg fetchAll )
 
 
 subscriptions : Model -> Sub Msg
@@ -18,15 +23,21 @@ subscriptions model =
     Sub.none
 
 
-
--- MAIN
+urlUpdate : Result String Route -> Model -> ( Model, Cmd Msg )
+urlUpdate result model =
+    let
+        currentRoute =
+            Routing.routeFromResult result
+    in
+        ( { model | route = currentRoute }, Cmd.none )
 
 
 main : Program Never
 main =
-    Html.App.program
+    Navigation.program Routing.parser
         { init = init
         , view = view
         , update = update
+        , urlUpdate = urlUpdate
         , subscriptions = subscriptions
         }
