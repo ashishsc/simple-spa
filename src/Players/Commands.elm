ce321fb00fd32ca680dbcac07b5b8b394669b3e2
Ashish Chandwani <ashish.s.chandwani@gmail.com>
2016-07-24 18:50:32 -0700

    Add Edit http saving logic

--- src/Players/Commands.elm
+++ src/Players/Commands.elm
@@ -2,6 +2,7 @@ module Players.Commands exposing (..)
 
 import Http
 import Json.Decode as Decode exposing ((:=))
+import Json.Encode as Encode
 import Task
 import Players.Models exposing (PlayerId, Player)
 import Players.Messages exposing (..)
@@ -29,3 +30,46 @@ memberDecoder =
         ("id" := Decode.int)
         ("name" := Decode.string)
         ("level" := Decode.int)
+
+
+saveUrl : PlayerId -> String
+saveUrl playerId =
+    "http://localhost:4000/players/" ++ (toString playerId)
+
+
+saveTask : Player -> Task.Task Http.Error Player
+saveTask player =
+    let
+        body =
+            memberEncoded player
+                |> Encode.encode 0
+                |> Http.string
+
+        config =
+            { verb = "PATCH"
+            , headers = [ ( "Content-Type", "application/json" ) ]
+            , url = saveUrl player.id
+            , body = body
+            }
+    in
+        Http.send Http.defaultSettings config
+            |> Http.fromJson memberDecoder
+
+
+save : Player -> Cmd Msg
+save player =
+    saveTask player
+        |> Task.perform SaveFail SaveSuccess
+
+
+memberEncoded : Player -> Encode.Value
+memberEncoded player =
+    let
+        list =
+            [ ( "id", Encode.int player.id )
+            , ( "name", Encode.string player.name )
+            , ( "level", Encode.int player.level )
+            ]
+    in
+        list
+            |> Encode.object
--- src/Players/Update.elm
+++ src/Players/Update.elm
@@ -1,7 +1,8 @@
 module Players.Update exposing (..)
 
 import Players.Messages exposing (Msg(..))
-import Players.Models exposing (Player)
+import Players.Commands exposing (save)
+import Players.Models exposing (Player, PlayerId)
 import Navigation
 
 
@@ -19,3 +20,36 @@ update action players =
 
         ShowPlayer id ->
             ( players, Navigation.modifyUrl ("#players/" ++ (toString id)) )
+
+        ChangeLevel id howMuch ->
+            ( players, changeLevelCommands id howMuch players |> Cmd.batch )
+
+        SaveSuccess updatedPlayer ->
+            ( updatePlayer updatedPlayer players, Cmd.none )
+
+        SaveFail error ->
+            ( players, Cmd.none )
+
+
+changeLevelCommands : PlayerId -> Int -> List Player -> List (Cmd Msg)
+changeLevelCommands playerId howMuch =
+    let
+        cmdForPlayer existingPlayer =
+            if (existingPlayer.id == playerId) then
+                save { existingPlayer | level = existingPlayer.level + howMuch }
+            else
+                Cmd.none
+    in
+        List.map cmdForPlayer
+
+
+updatePlayer : Player -> List Player -> List Player
+updatePlayer updatedPlayer =
+    let
+        select existingPlayer =
+            if existingPlayer.id == updatedPlayer.id then
+                updatedPlayer
+            else
+                existingPlayer
+    in
+        List.map select
